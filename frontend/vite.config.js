b3d9c18f92efebb864d989694894c2577e4e7af5
Edwin Mwiti <eduedwyn5@gmail.com>
2025-10-08 01:42:47 +0300

    Initial commit

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,23 +1,63 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  
   server: {
     port: 3000,
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
         changeOrigin: true,
+        secure: false,
       },
     },
   },
+  
   build: {
     outDir: 'dist',
     sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom'],
+          blockchain: ['@mysten/sui.js'],
+          charts: ['recharts'],
+          utils: ['axios', 'i18next']
+        }
+      }
+    }
   },
+  
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@components': resolve(__dirname, './src/components'),
+      '@pages': resolve(__dirname, './src/pages'),
+      '@hooks': resolve(__dirname, './src/hooks'),
+      '@utils': resolve(__dirname, './src/utils'),
+      '@services': resolve(__dirname, './src/services'),
+      '@context': resolve(__dirname, './src/context')
+    }
+  },
+  
   define: {
     'process.env': {}
-  }
-})
+  },
+  
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+    coverage: {
+      reporter: ['text', 'json', 'html'],
+      exclude: [
+        'node_modules/',
+        'src/setupTests.js',
+      ],
+    },
+  },
+});
